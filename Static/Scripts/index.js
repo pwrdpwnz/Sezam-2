b622125caf43abbd4591b6042082d1ece29a0cc4
pwrd <yeezuszxc@gmail.com>
2023-04-14 16:40:28 +0300

    end homepage

--- Static/Scripts/index.js
+++ Static/Scripts/index.js
@@ -1,5 +1,14 @@
+// Получение текущей даты
+const currentDate = new Date();
 
+// Получение текущего года
+const currentYear = currentDate.getFullYear();
 
+// Поиск элемента с классом ".date"
+const dateElement = document.querySelector(".date");
+
+// Установка значения текущего года в содержимое элемента
+dateElement.textContent = currentYear;
 
 
 // Добавляем обработчик событий для гамбургер меню
@@ -10,22 +19,4 @@ hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     menu.classList.toggle('active');
 });
-
-// Инициализация ScrollMagic
-var controller = new ScrollMagic.Controller();
-
-// Создание анимации с помощью TweenMax и TimelineMax
-var tween = new TimelineMax()
-    .add(TweenMax.to(".animated-text", 0.5, {opacity: 1, translateY: 0, ease: Power1.easeInOut})) // Анимация появления текста
-    .add(TweenMax.to(".animated-text", 0.5, {opacity: 0, translateY: -100, ease: Power1.easeInOut})); // Анимация исчезания текста
-
-// Создание сцены ScrollMagic
-new ScrollMagic.Scene({
-    triggerElement: ".animated-text", // Элемент, который должен появиться в видимости
-    triggerHook: 0.8, // Задержка перед началом анимации
-    reverse: true // Отключение обратного проигрывания анимации при скроллинге назад
-})
-    .setTween(tween) // Привязка анимации к сцене
-    .addIndicators() // Добавление индикаторов ScrollMagic Debug Indicator (можно удалить в финальной версии)
-    .addTo(controller); // Добавление сцены в контроллер ScrollMagic
 
